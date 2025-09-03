b2050c5faa30f68c02a2f5e82cffa22334e34c6d
FrancescoAntony <francesco31323@gmail.com>
2025-09-03 13:49:26 -0300

    Change: Adjustment-settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,9 +129,6 @@ STATIC_DIRS = [os.path.join(BASE_DIR, 'learning_logs/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'learning_logs/static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
